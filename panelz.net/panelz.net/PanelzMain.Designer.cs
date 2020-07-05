765f1f9c1ad37610d3942a3ff13eed7e8d8476da
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-07-05 13:23:10 +0530

    renamed frame

--- panelz.net/panelz.net/PanelzMain.Designer.cs
+++ panelz.net/panelz.net/PanelzMain.Designer.cs
@@ -28,10 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            // 
+            // PanelzMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "PanelzMain";
+            this.Text = "Panelz";
+
         }
 
         #endregion
